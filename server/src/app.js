f3c41711ad793633bd61286fc7abed98ad0e59cd
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-11-07 14:09:20 +0100

    fix: test coverage

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { listClients, listProducts } from "../dao.js";
+import { listClients, listProducts } from "./dao";
 
 import express from 'express';
 import morgan from 'morgan';
---
+++ server/src/dao.js
@@ -0,0 +1,49 @@
+"use strict";
+
+import db from "./db";
+
+export function listProducts() {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT p.id, pd.name, pd.description, pd.category, p.quantity, p.price
+            FROM Product p, Prod_descriptor pd
+            WHERE pd.id = p.ref_prod_descriptor`;
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const products = rows.map((p) => ({
+        id: p.id,
+        name: p.name,
+        description: p.description,
+        category: p.category,
+        quantity: p.quantity,
+        price: p.price,
+      }));
+      resolve(products);
+    });
+  });
+}
+
+export function listClients() {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT id, name, surname, phone, address, mail, balance
+            FROM  Client c`;
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const clients = rows.map((c) => ({
+        id: c.id,
+        name: c.name,
+        surname: c.surname,
+        phone: c.phone,
+        address: c.address,
+        mail: c.mail,
+        balance: c.balance,
+      }));
+      resolve(clients);
+    });
+  });
+}
