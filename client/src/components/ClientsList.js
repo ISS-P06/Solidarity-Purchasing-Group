940d7c81e2cc5ff01ae45b8f4176f87f64f11f83
_mary <mary_pal@live.it>
2021-11-07 11:05:45 +0100

    FE: add ClientsList and Client component.
    For the client component I have used a Card

---
+++ client/src/components/ClientsList.js
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import { Button, Row, Col, Spinner, ListGroup, Card } from 'react-bootstrap/';
+import { api_getClientsList } from "../Api";
+
+function ClientsList() {
+    const [clientsList, setClientsList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        api_getClientsList()
+            .then((result) => {
+                const tmp = [];
+                for (let client of result) {
+                    tmp.push(client)
+                }
+                setClientsList(tmp);
+                setLoading(false);
+            })
+            .catch((err) => {
+                /* TODO: handleErrors(err);  */
+                console.log(err);
+            });
+
+    }, []);
+
+    return (
+        loading ?
+            <Spinner animation='border' variant='primary' />
+            :
+            <Col lg="9">
+                <h3>Clients List</h3>
+                {clientsList.length === 0 ?
+                    <h1 className="text-center">There are not clients</h1> :
+                    <ListGroup as="ul" variant="flush" className="mt-2">
+                        {
+                            clientsList.map(c => {
+                                return (
+                                    <ListGroup.Item as="li" key={c.id}>
+                                        <Client client={c} />
+                                    </ListGroup.Item>
+                                );
+                            })
+                        }
+                    </ListGroup>
+
+                }
+
+            </Col>
+
+    );
+}
+
+
+function Client(props) {
+    const { client } = props;
+    return (
+        <Card className="text-left">
+            <Card.Header as="h5">{client.name + " " + client.surname}</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    Address: {client.address}
+                </Card.Text>
+
+                <Card.Text>
+                    E-mail: {client.mail}
+                </Card.Text>
+
+                <Card.Text>
+                    Phone number: {client.phone}
+                </Card.Text>
+
+                <Card.Text>
+                    Balance: {client.balance} $
+                </Card.Text>
+
+                <Row>
+                    <Col><Button variant="primary" className="mr-2">Top up wallet</Button></Col>
+                    <Col><Button variant="primary" className="mr-2">Add order</Button></Col>
+
+
+                </Row>
+
+
+            </Card.Body>
+        </Card>
+
+    );
+}
+export default ClientsList;
