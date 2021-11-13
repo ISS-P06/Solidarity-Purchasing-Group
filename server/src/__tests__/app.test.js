c98fc1105fcd6a4352626f7936c4c04f075de5cd
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-11-13 02:11:19 +0100

    fix: top up server api

--- server/src/__tests__/app.test.js
+++ server/src/__tests__/app.test.js
@@ -27,28 +27,28 @@ describe('Test the get virtual time clock', () => {
   });
 });
 
-describe("Test the clients api", () => {
-  test("It should respond to the GET method", () => {
-    return request(app).get("/api/clients").expect(200);
+describe('Test the clients api', () => {
+  test('It should respond to the GET method', () => {
+    return request(app).get('/api/clients').expect(200);
   });
 
-  test("It should respond to the PUT method", () => {
+  test('It should respond to the PUT method', () => {
     const id = 1;
     const amount = 100;
 
-    return request(app)
-      .put(`/api/clients/${id}/topup`)
-      .send({ amount })
-      .expect(200);
+    return request(app).put('/api/clients/topup').send({ amount, id }).expect(200);
   });
 
-  test("it should fail to the PUT methoh (low amount)", () => {
+  test('it should fail to the PUT methoh (low amount)', () => {
     const id = 1;
     const amount = 3;
 
-    return request(app)
-      .put(`/api/clients/${id}/topup`)
-      .send({ amount })
-      .expect(422);
+    return request(app).put('/api/clients/topup').send({ amount, id }).expect(422);
+  });
+
+  test('it should fail to the PUT methoh (missing parameter)', () => {
+    const amount = 50;
+
+    return request(app).put('/api/clients/topup').send({ amount }).expect(422);
   });
 });
