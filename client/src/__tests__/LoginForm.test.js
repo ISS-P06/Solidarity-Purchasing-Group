fe6dd2b6df07084e2d8699a0f2dfe4f36efe8c5f
Antonino Monti <antoninomonti99@gmail.com>
2021-11-15 11:51:51 +0100

    Updated LoginForm.test.js
    Updated test about filling out the login form with checks for error messages

--- client/src/__tests__/LoginForm.test.js
+++ client/src/__tests__/LoginForm.test.js
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { act } from 'react-dom/test-utils';
 import LoginForm from "../components/LoginForm";
+import { MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 describe("Test login form", () => {
     test("Test login form appearance", async () => {
@@ -19,11 +21,30 @@ describe("Test login form", () => {
     });    
 
     test("Test filling out login form", () => {
-        render(<LoginForm
-            doLogin={() => jest.fn}/>);
+        const history = createMemoryHistory();
+
+        // mock push function
+        history.push = jest.fn();
+
+        render(
+            <MemoryRouter history = {history}>
+                <LoginForm
+                    doLogin={() => jest.fn}/>
+            </MemoryRouter>
+        );
 
         //Click on the "Login" button without filling out fields
         act(() => {fireEvent.click(screen.getByText("Login"))});
+        // Check for error messages
+        expect(screen.getByText("Insert a username"));
+        expect(screen.getByText("Insert a valid password"));
+
+        // insert a password shorter than 8 characters
+        act(() => {fireEvent.change(screen.getByPlaceholderText("Password"), {
+            target: { value: 'p' },
+          })});
+        // Check for error message
+        expect(screen.getByText("Password must be at least 8 characters long"));
 
         // Fill out fields
         act(() => {fireEvent.change(screen.getByPlaceholderText("Username"), {
@@ -35,5 +56,7 @@ describe("Test login form", () => {
 
         //Click on the "Login" button after filling out fields
         act(() => {fireEvent.click(screen.getByText("Login"))});
+
+        fireEvent.click(screen.getByText("Back to home"));
     });
 });
