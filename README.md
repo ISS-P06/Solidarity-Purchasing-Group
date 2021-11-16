a2404b8fd4c47f483ea84d79c4643c9c72ae3af2
Salem Mohamed Salem Metwaly Abouzaid <s292508@studenti.polito.it>
2021-11-16 07:55:30 +0100

    nothing

--- README.md
+++
@@ -1,2 +0,0 @@
-# Solidarity-Purchasing-Group
-Solidarity Purchasing Group is a project developed for Software Engineering II course at Politecnico di Torino 
--- server/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-
-const port = 3001;
-
-// activate the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
