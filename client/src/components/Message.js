8a10e3befc4da95202a6afbff1def4460107bd00
_mary <mary_pal@live.it>
2021-11-22 17:26:22 +0100

    Subsostitute Alert with Toast notifications

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,35 +1,58 @@
-import { Alert } from 'react-bootstrap/';
-import { useEffect } from 'react';
+import ReactNotification from 'react-notifications-component';
 
-function AlertBox(props) {
-  const { alert, setAlert, message } = props;
+import { store } from 'react-notifications-component';
+import {useEffect} from "react";
 
-  // On componentDidMount set the timer
-  useEffect(() => {
-    if (alert) {
-      const timeId = setTimeout(() => {
-        // After 5 seconds set the show value to false
-        setAlert(false);
-      }, 5000);
-      return () => {
-        clearTimeout(timeId);
-      };
-    }
-  }, [alert, setAlert]);
-
-  if (alert) {
-    return (
-      <Alert
-        className="mt-3 pb-0"
-        variant={message.type}
-        onClose={() => setAlert(false)}
-        dismissible>
-        <p>{message.msg}</p>
-      </Alert>
-    );
-  } else {
-    return <> </>;
-  }
+export function addMessage(title,message,type) {
+    console.log("add");
+    console.log(message);
+    console.log(type);
+    store.addNotification({
+        title: title || "",
+        message: message,
+        type: type,
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+            duration: 5000,
+            onScreen: true
+        }
+    })
 }
 
-export default AlertBox;
+function Notification(props) {
+    // const { alert, setAlert, message } = props;
+
+    // On componentDidMount set the timer
+    // useEffect(() => {
+    //   if (alert) {
+    //     const timeId = setTimeout(() => {
+    //       // After 5 seconds set the show value to false
+    //       setAlert(false);
+    //     }, 5000);
+    //     return () => {
+    //       clearTimeout(timeId);
+    //     };
+    //   }
+    // }, [alert, setAlert]);
+
+    useEffect(()=>{
+
+    },[])
+
+    return (
+        <ReactNotification/>
+        // <Alert
+        //   className="mt-3 pb-0"
+        //   variant={message.type}
+        //   onClose={() => setAlert(false)}
+        //   dismissible>
+        //   <p>{message.msg}</p>
+        // </Alert>
+    );
+
+}
+
+export default Notification;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,5 @@ export * from './order';
 export { default as AppNavbar } from './AppNavbar';
 export { default as ProductCards } from './ProductCards';
 export { default as ShopEmployeeActionsList } from './ShopEmployeeActionsList';
-export { default as AlertBox } from './Message';
+export { default as Notification } from './Message';
 export { default as LoginForm } from './LoginForm';
