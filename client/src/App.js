15378996e3cd3dc01fffdd1a28810364a5f147ed
_mary <mary_pal@live.it>
2021-11-23 21:17:51 +0100

    - Add more validation - Update design - Add input for select the type of registration (client,farmer,shop_employee)

--- client/src/App.js
+++ client/src/App.js
@@ -132,6 +132,9 @@ function App() {
               <Route path="/login">
                 {loggedIn ? <RedirectUser userRole={userRole} /> : <LoginForm doLogin={doLogin} />}
               </Route>
+              <Route path="/register">
+                {!loggedIn ?  <InsertClient loggedIn={loggedIn}/>  : "" } {/*TODO: page of the user registered*/}
+              </Route>
               {/* Shop employee-only routes */}
 
               {/* Employee: client info page route */}
@@ -157,7 +160,7 @@ function App() {
               {/* Employee client registration route */}
               <Route path="/employee/register">
                 {loggedIn && userRole == 'shop_employee' ? (
-                  <InsertClient />
+                  <InsertClient loggedIn={loggedIn}/>
                 ) : (
                   <RedirectUser userRole={userRole} />
                 )}
