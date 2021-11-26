ee9c9b2c42d1a8cfb60ae0544ca8b8ff6e8a6490
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-11-26 14:36:07 +0100

    fix: navbar login

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,48 +1,52 @@
-import { Navbar as BNavbar, Nav } from 'react-bootstrap';
-import { BsBasket2 } from 'react-icons/bs';
+import { Navbar as BNavbar, Dropdown } from 'react-bootstrap';
+import { BsBasket2, BsPersonCircle } from 'react-icons/bs';
+import { BiLogIn, BiLogOut } from 'react-icons/bi';
+
 import { Link } from 'react-router-dom';
 
 import { getUserRoute } from '../utils';
 
 function Navbar(props) {
-  const loggedIn = props.loggedIn;
-  const doLogout = props.doLogout;
   const userRole = props.userRole;
-
-  const userPageText = (role) => {
-    switch (role) {
-      case 'shop_employee':
-        return 'Employee page';
-      default:
-        return 'Home page';
-    }
-  };
 
   return (
     <BNavbar collapseOnSelect className="navbar text-light" fixed="top" fluid="true" expand="lg">
-      <BNavbar.Toggle className="mr-auto" aria-controls="responsive-navbar-nav" />
-
-      <BNavbar.Brand as={Link} to="/" style={{ color: '#fff' }}>
-        <BsBasket2 size={30} /> Solidarity Purchasing Group
+      <BNavbar.Brand as={Link} to={getUserRoute(userRole)} style={{ color: '#fff' }}>
+        <BsBasket2 size={30} /> SPG
       </BNavbar.Brand>
 
-      <BNavbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-        <Nav className="text-light">
-          {loggedIn ? (
-            <>
-              <Nav.Link as={Link} to={getUserRoute(userRole)}>
-                Home Page
-              </Nav.Link>
-              <Nav.Link onClick={doLogout}>Logout</Nav.Link>
-            </>
-          ) : (
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-          )}
-        </Nav>
-      </BNavbar.Collapse>
+      <BNavbar.Collapse
+        id="responsive-navbar-nav"
+        className="justify-content-end"></BNavbar.Collapse>
+
+      <SettingsDropdown {...props} />
     </BNavbar>
+  );
+}
+
+function SettingsDropdown(props) {
+  const { loggedIn, doLogout } = props;
+
+  return (
+    <Dropdown drop="start" className="settings-dd">
+      <Dropdown.Toggle variant="success">
+        <BsPersonCircle size={20} className="my-1" />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {!loggedIn ? (
+          <Dropdown.Item as={Link} to="/login">
+            <BiLogIn size={25} />
+            Login
+          </Dropdown.Item>
+        ) : (
+          <Dropdown.Item onClick={doLogout}>
+            <BiLogOut size={25} />
+            Logout
+          </Dropdown.Item>
+        )}
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
 
