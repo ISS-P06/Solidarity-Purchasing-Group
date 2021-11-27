0c83a3ebc61548493a0cff71f1c4ce49fd3b1882
Antonino Monti <antoninomonti99@gmail.com>
2021-11-27 17:13:06 +0100

    feat(BE): added tests for SYS class
    Added tests for the new SYS class; testing required adding a new method to insert dummy orders in the DB

---
+++ server/src/__tests__/system.test.js
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import app from '../app';
+
+import {test_addDummyOrders} from '../system-dao';
+
+import SYS from '../system';
+
+const sys = new SYS();
+
+/** TEST SUITES */
+describe("Test SYS class: trigger for insufficient balance notification", () => {
+    beforeAll(async () => {
+        // set virtual time to a Monday at 9am
+        let today = new Date();
+        let today_9am = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 6, 0, 0, 0);
+
+        let monday9am = today_9am;
+
+        while (monday9am.getDay() !== 0) {
+            monday9am.setDate(monday9am.getDate() + 1);
+        }
+
+        request(app).put('/api/time').send(monday9am.toISOString()).expect(200);
+
+        await test_addDummyOrders();
+    });
+
+    test("Everything should work", () => {
+        sys.event_updateOrders();
+
+        let mailingList = [
+            {
+                email: "test@test.com",
+                id: -1
+            }
+        ];
+
+        sys.event_sendBalanceReminders(mailingList);
+    });
+});
