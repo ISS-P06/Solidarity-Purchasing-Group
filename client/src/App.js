13a846ee915cab15546ddb0b89a2464c4d0f02a2
Antonino Monti <antoninomonti99@gmail.com>
2021-11-27 13:12:11 +0100

    feat: integrated VTC for orders
    Integrated VTC so that orders can only be made within a certain time frame

--- client/src/App.js
+++ client/src/App.js
@@ -14,32 +14,252 @@ import {
     Basket,
     ClientHomePage,
 } from './components';
-import HomePage from './containers/HomePage';
-import {Layout} from './containers';
-import {getUserRoute, RedirectRoute} from './utils/route.js';
-import {addMessage} from './components/Message';
-import {api_getUserInfo, api_login, api_logout} from './Api';
-import FarmerHomePage from "./components/farmer/FarmerHomePage";
-function App() {
-    // Session-related states
-    const [loggedIn, setLoggedIn] = useState(false);
-    /*
-        userRole: current user's role; possible values:
-        - shop_employee
-        - (empty string/none, i.e. not logged in)
 
-        other values will be considered in subsequent sprints
-        when necessary
-      */
-    const [userRole, setUserRole] = useState('');
-    const [userId, setUserId] = useState();
-    const [user, setUser] = useState();
-    const [toggled, setToggled] = useState(false);
-    const [collapsed, setCollapsed] = useState(false);
-  
-   const handleCollapsedChange = (checked) => {
-        setCollapsed(checked);
+import Basket from './components/order/Basket'
+
+import { api_getUserInfo, api_login, api_logout, api_getTime } from './Api';
+
+function App() {
+  // Session-related states
+  const [loggedIn, setLoggedIn] = useState(false);
+  /*
+    userRole: current user's role; possible values:
+    - shop_employee
+    - (empty string/none, i.e. not logged in)
+
+    other values will be considered in subsequent sprints
+    when necessary
+  */
+  const [userRole, setUserRole] = useState('');
+  const [userId, setUserId] = useState();
+
+  // This state is used to update (and monitor) the time on front-end
+  // Some functionalities can be used only at a certain time
+  const [dirtyVT, setDirtyVT] = useState(true);
+
+  // State used to store the system's virtual time
+  const [virtualTime, setVirtualTime] = useState({});
+
+  /* for giving feedback to the user*/
+  const [message, setMessage] = useState('');
+  const [alert, setAlert] = useState(false);
+
+  const [toggled, setToggled] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleCollapsedChange = (checked) => {
+    setCollapsed(checked);
+  };
+
+  const handleToggleSidebar = (value) => {
+    setToggled(value);
+  };
+
+  // useEffect for getting user info
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const info = await api_getUserInfo();
+        setLoggedIn(true);
+        setUserRole(info.role);
+        setUserId(info.id);
+      } catch (err) {
+        setUserRole('');
+        console.error(err);
+      }
     };
+    checkAuth();
+  }, [loggedIn]);
+
+  // useEffect used to get the system's virtual time
+  useEffect(() => {
+    const getVT = async () => {
+      try {
+        const data = await api_getTime();
+        console.log(data);
+        setVirtualTime(new Date(data.currentTime));
+        setDirtyVT(false);
+      } catch (err) {
+        setVirtualTime(new Date().toISOString());
+        setDirtyVT(false);
+        console.error(err);
+      }
+    };
+    getVT();
+  }, [dirtyVT]);
+
+  // async function for logging in
+  const doLogin = async (credentials) => {
+    try {
+      await api_login(credentials);
+      setLoggedIn(true);
+      return { done: true, msg: 'ok' };
+    } catch (err) {
+      return { done: false, msg: err.message };
+    }
+  };
+
+  // async function for logging out
+  const doLogout = async () => {
+    await api_logout();
+    setLoggedIn(false);
+  };
+
+  // useEffect for getting user info
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const info = await api_getUserInfo();
+        setLoggedIn(true);
+        setUserRole(info.role);
+      } catch (err) {
+        setUserRole('');
+        console.error(err);
+      }
+    };
+    checkAuth();
+  }, [loggedIn]);
+
+  return (
+    <Container className="App text-dark p-0 m-0 min-vh-100" fluid="true">
+      <Router>
+        <AppNavbar 
+          loggedIn={loggedIn} 
+          doLogout={doLogout} 
+          userRole={userRole} 
+          dirtyVT={dirtyVT}
+          setDirtyVT={setDirtyVT} 
+          virtualTime={virtualTime}
+          />
+        <Notification />
+        {/*<AlertBox alert={alert} setAlert={setAlert} message={message} />*/}
+        
+
+
+        <Row className="m-auto">
+          {loggedIn && userRole == 'shop_employee' ? (
+            <Col xs={1} md={4} lg={2} className="p-0">
+              {/* This button shows up when the sidebar is hidden */}
+              <Button
+                className="btn-toggle m-2"
+                onClick={() => handleToggleSidebar(true)}
+                className={toggled ? 'd-none' : 'btn-toggle'}>
+                <FaBars />
+              </Button>
+
+              {/* Aside */}
+              <ShopEmployeeActionsList
+                toggled={toggled}
+                collapsed={collapsed}
+                handleToggleSidebar={handleToggleSidebar}
+
+              />
+            </Col>
+          ) : (
+            <div />
+          )}
+
+          {/*<Col xs={11} md={8} lg={10}>*/}
+          <Col>
+            <Switch>
+              {/* Login route */}
+              <Route path="/login">
+                {loggedIn ? <RedirectUser userRole={userRole} /> : <LoginForm doLogin={doLogin} />}
+              </Route>
+              <Route path="/register">
+                {!loggedIn ?  <InsertClient loggedIn={loggedIn}/>  : "" } {/*TODO: page of the user registered*/}
+              </Route>
+              {/* Shop employee-only routes */}
+
+              {/* Employee: client info page route */}
+              <Route
+                path="/employee/clients/:id"
+                render={({ match }) => {
+                  if (loggedIn) {
+                    return <div id={match.params.id} />;
+                  } else {
+                    return <RedirectUser userRole={userRole} />;
+                  }
+                }}
+              />
+              {/* Employee client list route */}
+              <Route path="/employee/clients">
+                {loggedIn && userRole === 'shop_employee' ? (
+                  <ClientsList 
+                    setMessage={setMessage} 
+                    virtualTime={virtualTime} />
+                ) : (
+                  <RedirectUser userRole={userRole} />
+                )}
+              </Route>
+
+              {/* Employee client registration route */}
+              <Route path="/employee/register">
+                {loggedIn && userRole == 'shop_employee' ? (
+                  <InsertClient loggedIn={loggedIn}/>
+                ) : (
+                  <RedirectUser userRole={userRole} />
+                )}
+              </Route>
+
+              {/* Employee product browsing route */}
+              <Route path="/employee/products">
+                {loggedIn && userRole == 'shop_employee' ? (
+                  <ProductCards />
+                ) : (
+                  <RedirectUser userRole={userRole} />
+                )}
+              </Route>
+
+              {/* Employee: order info page route */}
+              <Route path="/employee/orders/:id">
+                {loggedIn ? <OrderReview /> : <RedirectUser userRole={userRole} />}
+              </Route>
+
+              {/* Employee order list route */}
+              <Route path="/employee/orders">
+                {loggedIn && userRole == 'shop_employee' ? (
+                  <OrderList />
+                ) : (
+                  <RedirectUser userRole={userRole} />
+                )}
+              </Route>
+
+              {/* Employee order creation route */}
+              <Route path="/employee/orders/new">
+                {loggedIn && userRole == 'shop_employee' ? (
+                  <div />
+                ) : (
+                  <RedirectUser userRole={userRole} />
+                )}
+              </Route>
+
+              {/* Employee home page route */}
+              <Route path="/employee">
+                {loggedIn && userRole == 'shop_employee' ? (
+                  <div />
+                ) : (
+                  <RedirectUser userRole={userRole} />
+                )}
+              </Route>
+
+              {/* Home page route */}
+              <Route path="/">
+                {/* Replace div with homepage component */}
+                <div />
+                <Basket userId={userId}/>
+              </Route>
+
+              <Route>
+                <Redirect to="/" />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Router>
+    </Container>
+  );
+}
 
     const handleToggleSidebar = (value) => {
         setToggled(value);
--- client/src/components/client/ClientsList.js
+++ client/src/components/client/ClientsList.js
@@ -4,9 +4,13 @@ import { Button, Row, Col, Spinner, Card, Modal, Container } from 'react-bootstr
 import { api_getClientsList, api_addTopUpClient } from '../../Api';
 import ClientOrderForm from './ClientOrderForm';
 import ClientTopUpForm from './ClientTopUpForm';
-import { addMessage } from '../Message';
+import {addMessage} from "../Message";
+import { checkOrderInterval } from '../../utils/date.js';
 
-function ClientsList() {
+function ClientsList(props) {
+  const { setMessage } = props;
+  const virtualTime = props.virtualTime;
+
   const [clientsList, setClientsList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [dirty, setDirty] = useState(true);
@@ -38,7 +42,7 @@ function ClientsList() {
             <div as="ul" variant="flush">
               {clientsList.map((c) => (
                 <div as="li" className="mt-1 mb-4" key={c.id} lg={3}>
-                  <Client client={c} reloadList={() => setDirty(true)} />
+                  <Client client={c} setMessage={setMessage} virtualTime={virtualTime} reloadList={() => setDirty(true)} />
                 </div>
               ))}
             </div>
@@ -50,7 +54,7 @@ function ClientsList() {
 }
 
 export function Client(props) {
-  const { client, reloadList } = props;
+  const { client, setMessage, reloadList, virtualTime } = props;
 
   const [clientOrderFormShow, setClientOrderFormShow] = useState(false);
   const [clientTopUpFormShow, setClientTopUpFormShow] = useState(false);
@@ -59,7 +63,7 @@ export function Client(props) {
   const handleTopUp = (params) => {
     api_addTopUpClient(params)
       .then(() => reloadList())
-      .catch(() => {});
+      .catch(() => { });
   };
 
   return (
@@ -78,11 +82,26 @@ export function Client(props) {
             </Button>
           </Col>
           <Col>
-            <Button className="btn mr-2" onClick={() => setClientOrderFormShow(true)}>
-              Add order
-            </Button>
+            {checkOrderInterval(virtualTime) ? (
+              <Button className="btn mr-2" onClick={() => setClientOrderFormShow(true)}>
+                Add order
+              </Button>) :
+              (
+                <Button className="btn mr-2" onClick={() => setClientOrderFormShow(true)} disabled>
+                  Add order
+                </Button>
+              )
+            }
           </Col>
         </Row>
+        {
+            checkOrderInterval(virtualTime) ? (<></>) :
+              (
+                <Row className="p-1 d-flex justify-content-center">
+                  Sorry, but orders are accepted only from Sat. 9am until Sun. 11pm.
+                </Row>
+              )
+          }
       </Card.Body>
       <ClientOrderForm
         show={clientOrderFormShow}
