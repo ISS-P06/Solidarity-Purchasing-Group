b0373f17d621c7688d442728f9b78b5a9bacb2ab
_mary <mary_pal@live.it>
2021-12-03 22:32:45 +0100

    Added new functionalities as icons in the homepage

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,33 +1,37 @@
 import {Carousel, Image, Row, Col, Container} from 'react-bootstrap';
-import {BsFillPersonPlusFill,BsBox} from 'react-icons/bs';
+import {BsFillPersonPlusFill, BsBox} from 'react-icons/bs';
 import {ImBoxAdd, ImBoxRemove} from 'react-icons/im';
 import {GiWallet} from 'react-icons/gi';
 import {RiShoppingBasketFill} from 'react-icons/ri';
+import {GiFruitBowl, GiFruitTree} from 'react-icons/gi';
 
 function HomePage() {
     const image1 = '/img/homepage/Agriculture.jpg';
     const image2 = '/img/homepage/Strawberry.jpg';
     const image3 = '/img/homepage/Zucchini.jpg';
 
-
     return (
         <Container>
             <Row className="justify-content-center">
                 <Col xs={0} md={10}>
-                    <h1 style={{color:"#27511D"}}>Welcome on Solidarity Purchase Group!</h1>
-                    <Carousel className="mt-3 d-none d-md-block" >
+                    <h1 style={{color: "#27511D"}}>Welcome on Solidarity Purchase Group!</h1>
+                    <Carousel className="mt-3 d-none d-md-block">
                         <Carousel.Item>
-                            <Image src={image1} className="img" alt="A non profit organization" style={{width: "60%", paddingTop: "20px"}}/>
+                            <Image src={image1} className="img" alt="A non profit organization"
+                                   style={{width: "60%", paddingTop: "20px"}}/>
                             <Carousel.Caption>
                                 <Container>
                                     <Row>
                                         <Col xs={10} md={{offset: 2, span: 8}}>
-                                            <h2 className="pb-2" style={{background:"#839d7c"}}>A non profit organization</h2>
+                                            <h2 className="pb-2" style={{background: "#839d7c"}}>A non profit
+                                                organization</h2>
                                         </Col>
                                     </Row>
                                     <Row>
                                         <Col md={{offset: 1, span: 10}}>
-                                            <p className="pt-2 pb-2" style={{background:"#839d7c",    fontStyle: "italic"}}>Support your own local farmers with our
+                                            <p className="pt-2 pb-2"
+                                               style={{background: "#839d7c", fontStyle: "italic"}}>Support your own
+                                                local farmers with our
                                                 app, every purchase helps them grow!</p>
                                         </Col>
                                     </Row>
@@ -35,17 +39,21 @@ function HomePage() {
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
-                            <Image src={image2} className="img" alt="Solidarity among clients" style={{width: "60%", paddingTop: "20px"}}/>
+                            <Image src={image2} className="img" alt="Solidarity among clients"
+                                   style={{width: "60%", paddingTop: "20px"}}/>
                             <Carousel.Caption>
                                 <Container>
                                     <Row>
                                         <Col xs={12} md={{offset: 2, span: 8}}>
-                                            <h2 className="pb-2" style={{background:"#839d7c"}}>Solidarity among clients</h2>
+                                            <h2 className="pb-2" style={{background: "#839d7c"}}>Solidarity among
+                                                clients</h2>
                                         </Col>
                                     </Row>
                                     <Row>
                                         <Col md={{offset: 1, span: 10}}>
-                                            <p className="pt-2 pb-2" style={{background:"#839d7c",    fontStyle: "italic"}}>We believe collaboration is the key to
+                                            <p className="pt-2 pb-2"
+                                               style={{background: "#839d7c", fontStyle: "italic"}}>We believe
+                                                collaboration is the key to
                                                 reducing food waste</p>
                                         </Col>
                                     </Row>
@@ -53,17 +61,21 @@ function HomePage() {
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
-                            <Image src={image3} className="img" alt="Sustainable agriculture" style={{width: "60%", paddingTop: "20px"}} />
+                            <Image src={image3} className="img" alt="Sustainable agriculture"
+                                   style={{width: "60%", paddingTop: "20px"}}/>
                             <Carousel.Caption>
                                 <Container>
                                     <Row>
                                         <Col xs={12} md={{offset: 2, span: 8}}>
-                                            <h2 className="pb-2" style={{background:"#839d7c"}}>Sustainable agriculture</h2>
+                                            <h2 className="pb-2" style={{background: "#839d7c"}}>Sustainable
+                                                agriculture</h2>
                                         </Col>
                                     </Row>
                                     <Row>
                                         <Col md={{offset: 1, span: 10}}>
-                                            <p className="pt-2 pb-2" style={{background:"#839d7c",    fontStyle: "italic"}}>Discover healthy and seasonal products from
+                                            <p className="pt-2 pb-2"
+                                               style={{background: "#839d7c", fontStyle: "italic"}}>Discover healthy and
+                                                seasonal products from
                                                 your area</p>
                                         </Col>
                                     </Row>
@@ -72,11 +84,11 @@ function HomePage() {
                         </Carousel.Item>
                     </Carousel>
                 </Col>
-                <h3 className={"mt-5"} >
+                <h3 className={"mt-5"}>
                     We are a self-organized group of citizens and farmers that try to promote a sustainable and
                     high-quality network of food in order to buy and sell products at a fair price.
-                </h3 >
-                <h1 className="pt-3"  style={{color:"#27511D"}}>Check out our new features!</h1>
+                </h3>
+                <h1 className="pt-3" style={{color: "#27511D"}}>Check out our new features!</h1>
                 <h3 className="m-3 pb-2 pt-2">Services for shop employees</h3>
                 <Row className="justify-content-center">
                     <Col xs={6} lg={3}>
@@ -113,6 +125,12 @@ function HomePage() {
                         </h5>
                     </Col>
                     <Col xs={6} lg={3}>
+                        <GiFruitBowl className="icon"/>
+                        <h5>
+                            Browse the products available
+                        </h5>
+                    </Col>
+                    <Col xs={6} lg={3}>
                         <RiShoppingBasketFill className="icon"/>
                         <h5>
                             Add product to basket
@@ -121,14 +139,25 @@ function HomePage() {
                     <Col xs={6} lg={3}>
                         <BsBox className="icon"/>
                         <h5>
-                          Browse orders
+                            Browse orders
                         </h5>
                     </Col>
+
+                </Row>
+                <h3 className="m-3 pt-1">Services for farmers</h3>
+                <Row className="justify-content-center">
+                    <Col xs={6} lg={3}>
+                        <GiFruitTree className="icon"/>
+                        <h5>
+                            Report expected available product amounts
+                        </h5>
+                    </Col>
+
                 </Row>
             </Row>
         </Container>
     )
-        
+
 }
 
 export default HomePage;
