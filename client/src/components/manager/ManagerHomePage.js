2700cd6c71e90d5e75ebf3f44ec1ba1a9dc02599
Marco Ballario <marco.ballario1997@gmail.com>
2022-01-05 00:14:38 +0100

    Add datepicker and manager routes

--- client/src/components/manager/ManagerHomePage.js
+++ client/src/components/manager/ManagerHomePage.js
@@ -1,51 +1,55 @@
 import { useState, useEffect } from 'react';
-import { api_generateMonthlyReport, api_generateWeeklyReport } from "../../Api";
+import { api_generateMonthlyReport, api_generateWeeklyReport, api_getTime } from "../../Api";
 import dayjs from 'dayjs';
 import { Col, Row, Container } from 'react-bootstrap';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { PieChart, Pie } from 'recharts';
-import { Card, Table } from 'react-bootstrap';
+import { Card, Table, Form, InputGroup } from 'react-bootstrap';
 
 function ManagerHomePage(props) {
-  const { user } = props;
+  const { user, type, virtualTime } = props;
   const [x, setX] = useState("");
+  const [y, setY] = useState((dayjs(virtualTime)).format("YYYY-MM-DD"));
+  const cardHeight = '14.5rem';
 
-  const data = [
-    { name: 'Delivered', number: 90 },
-    { name: 'Undelivered', number: 10 },
-  ];
+  /*
+  DUMMY STATS
+  let abc = {
+    totalOrders: 100,
+    deliveredOrders: 90,
+    undeliveredOrders: 10,
+    totalFood: 100,
+    deliveredFood: 75,
+    undeliveredFood: 25,
+    perc_undeliveredOrd: 0.1,
+    perc_deliveredOrd: 0.9,
+    perc_undeliveredFood: 0.25,
+    perc_deliveredFood: 0.75,
+  }
+  */
 
   useEffect(() => {
-    const date = dayjs(new Date("November, 16 2021 00:00:00")).format("YYYY-MM-DD HH:mm");
-    api_generateMonthlyReport(date)
-      .then((response) => {
-        let abc = {
-          totalOrders: 100,
-          deliveredOrders: 90,
-          undeliveredOrders: 10,
-          totalFood: 100,
-          deliveredFood: 75,
-          undeliveredFood: 25,
-          perc_undeliveredOrd: 0.1,
-          perc_deliveredOrd: 0.9,
-          perc_undeliveredFood: 0.25,
-          perc_deliveredFood: 0.75,
-        }
-        setX(abc);
+    if (y !== (dayjs(virtualTime)).format("YYYY-MM-DD")) {
+      api_generateWeeklyReport(dayjs(y).format("YYYY-MM-DD HH:mm"))
+        .then((response) => {
+          setX(response);
+        })
+        .catch((error) => {
 
-      })
-      .catch((error) => {
-
-      });
-  }, []);
-
-  const cardHeight = '14.5rem';
+        });
+    }
+  }, [y]);
 
   return (<>
     <Container className="mt-0">
       <Row className="mb-3">
         <Col style={{ display: 'flex', justifyContent: 'center' }}>
-          <h3>Weekly report</h3>
+          <div style={{ paddingRight: 15 }}><h3>{type} report for date: </h3></div>
+          <Form>
+            <InputGroup>
+              <Form.Control type="date" value={y} onChange={(e) => setY(e.target.value)}></Form.Control>
+            </InputGroup>
+          </Form>
         </Col>
       </Row>
       <Row className="mb-4">
@@ -90,11 +94,19 @@ function StatisticsTable(props) {
   const { stats, type } = props;
   const leftSpace = 10;
 
-  let version;
+  let tot, del, und, pdel, pund;
   if (type === "Orders") {
-    version = 1;
+    tot = stats.totalOrders ? stats.totalOrders : 0;
+    del = stats.deliveredOrders ? stats.deliveredOrders : 0;
+    und = stats.undeliveredOrders ? stats.undeliveredOrders : 0;
+    pdel = stats.perc_deliveredOrd ? stats.perc_deliveredOrd * 100 + " %" : 0 + " %";
+    pund = stats.perc_undeliveredOrd ? stats.perc_undeliveredOrd * 100 + " %" : 0 + " %";
   } else if (type === "Food") {
-    version = 0;
+    tot = stats.totalFood ? stats.totalFood + " Kg" : 0 + " Kg";
+    del = stats.deliveredFood ? stats.deliveredFood + " Kg" : 0 + " Kg";
+    und = stats.undeliveredFood ? stats.undeliveredFood + " Kg" : 0 + " Kg";
+    pdel = stats.perc_deliveredFood ? stats.perc_deliveredFood * 100 + " %" : 0 + " %";
+    pund = stats.perc_undeliveredFood ? stats.perc_undeliveredFood * 100 + " %" : 0 + " %";
   }
 
   return (
@@ -108,23 +120,23 @@ function StatisticsTable(props) {
       <tbody>
         <tr>
           <td className="text-start" style={{ paddingLeft: leftSpace }}>Total</td>
-          <td>{version ? stats.totalOrders : stats.totalFood + " Kg"}</td>
+          <td>{tot}</td>
         </tr>
         <tr>
           <td className="text-start" style={{ paddingLeft: leftSpace }}>Delivered</td>
-          <td>{version ? stats.deliveredOrders : stats.deliveredFood + " Kg"}</td>
+          <td>{del}</td>
         </tr>
         <tr>
           <td className="text-start" style={{ paddingLeft: leftSpace }}>Undelivered</td>
-          <td>{version ? stats.undeliveredOrders : stats.undeliveredFood + " Kg"}</td>
+          <td>{und}</td>
         </tr>
         <tr>
           <td className="text-start" style={{ paddingLeft: leftSpace }}>Percentage delivered</td>
-          <td>{version ? stats.perc_deliveredOrd * 100 : stats.perc_deliveredFood * 100} %</td>
+          <td>{pdel}</td>
         </tr>
         <tr className="p-0">
           <td className="text-start" style={{ paddingLeft: leftSpace }}>Percentage undelivered</td>
-          <td>{version ? stats.perc_undeliveredOrd * 100 : stats.perc_undeliveredFood * 100} %</td>
+          <td>{pund}</td>
         </tr>
       </tbody>
     </Table>
@@ -138,10 +150,16 @@ function CustomBarChart(props) {
   let labelText;
   let delivered, undelivered;
   if (type === "Orders") {
+    if (!stats.totalOrders) {
+      return <div className="p-5">No orders data available.</div>;
+    }
     labelText = "Number of orders";
     delivered = stats.deliveredOrders;
     undelivered = stats.undeliveredOrders;
   } else if (type === "Food") {
+    if (!stats.totalFood) {
+      return <div className="p-5">No food data available.</div>;
+    }
     labelText = "Quantity of food (Kg)";
     delivered = stats.deliveredFood;
     undelivered = stats.undeliveredFood;
@@ -176,10 +194,16 @@ function CustomPieChart(props) {
   let labelText;
   let delivered, undelivered;
   if (type === "Orders") {
+    if (!stats.totalOrders) {
+      return <div className="p-5">No orders data available.</div>;
+    }
     labelText = " orders (%)";
     delivered = stats.perc_deliveredOrd * 100;
     undelivered = stats.perc_undeliveredOrd * 100;
   } else if (type === "Food") {
+    if (!stats.totalFood) {
+      return <div className="p-5">No food data available.</div>;
+    }
     labelText = " food (%)";
     delivered = stats.perc_deliveredFood * 100;
     undelivered = stats.perc_undeliveredFood * 100;
--- client/src/components/routes/RoutesManager.js
+++ client/src/components/routes/RoutesManager.js
@@ -1,23 +1,43 @@
 import { RedirectRoute } from '../../utils/route';
 import { LoginForm} from '../';
 import { ManagerHomePage } from '../manager';
-import ProductCards from '../product/ProductCards';
 
 /**
- *  This component contains all farmer-only routes.
+ *  This component contains all manager-only routes.
  */
 function RoutesManager(props) {
     const {loggedIn, userRole, doLogin, userId, user, virtualTime} = props;
     console.log(props);
     return (
         <>
-            {/* Farmer home page */}
+            {/* --- Manager-only routes --- */}
+            {/* Manager weekly report route */}
+            <RedirectRoute
+              path="/manager/report/weekly"
+              exact={true}
+              role={userRole}
+              condition={loggedIn && userRole === 'manager'}
+              component={<ManagerHomePage user={user} type="Weekly" virtualTime={virtualTime} />}
+              redirect={<LoginForm doLogin={doLogin} />}
+            />
+            
+            {/* Manager monthly report route */}
+            <RedirectRoute
+              path="/manager/report/monthly"
+              exact={true}
+              role={userRole}
+              condition={loggedIn && userRole === 'manager'}
+              component={<ManagerHomePage user={user} type="Monthly" virtualTime={virtualTime} />}
+              redirect={<LoginForm doLogin={doLogin} />}
+            />
+
+            {/* Manager home page */}
             <RedirectRoute
               path="/manager"
               exact={true}
               role={userRole}
               condition={loggedIn && userRole === 'manager'}
-              component={<ManagerHomePage user={user} />}
+              component={<div>To do: put here manager homepage</div>}
               redirect={<LoginForm doLogin={doLogin} />}
             />
         </>
